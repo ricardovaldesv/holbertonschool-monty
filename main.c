fa1115fdf49b94f9a81b8b9b06782a9203c1aa7c
ricardovaldesv <ricardovaldes@icloud.com>
2023-09-06 09:12:07 -0700

    0. push, pall v2

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 
         if (argc != 2)
         {
-                fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+                fprintf(stderr, "USAGE: monty file\n");
                 exit(EXIT_FAILURE);
         }
         file = fopen(argv[1], "r");
