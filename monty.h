25171240459a44ee02fc5ca228cbab39a42a22e9
ricardovaldesv <ricardovaldes@icloud.com>
2023-09-07 08:01:58 -0700

    betty monty.h

--- monty.h
+++ monty.h
@@ -16,9 +16,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -31,12 +31,13 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 stack_t *stack = NULL;
 void push(stack_t **stack, int value);
 void pall(stack_t **stack, unsigned int line_number);
 void free_dlistint(stack_t *stack);
+
 #endif
