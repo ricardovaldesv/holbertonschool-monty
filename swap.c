ceca32e0125968ce2859537f574b29713b33327b
ricardovaldesv <ricardovaldes@icloud.com>
2023-09-08 09:34:59 -0700

    refactor swap v2

--- swap.c
+++ swap.c
@@ -9,27 +9,12 @@
 
 void swap(stack_t **stack, unsigned int line_number)
 {
-	size_t node;
-
-	node = dlistint_len(*stack);
-        stack_t *node_to_swap = *stack;
+	int temp2;
+	stack_t *node_to_swap = *stack;
 	(void)line_number;/*Parametro no Utilizado*/
-	if (node == 2)
-	{
-		*stack = node_to_swap->next;
-		(*stack)->prev = NULL;
-		node_to_swap->prev = node_to_swap->next;
-		(*stack)->next = node_to_swap;
-		node_to_swap->next = NULL;
-		
-	}
-	else
-	{
-        	*stack = node_to_swap->next;
-        	(*stack)->prev = NULL;
-        	node_to_swap->prev = (*stack)->next;
-        	(*stack)->next->prev = node_to_swap;
-        	node_to_swap->next = (*stack)->next;
-        	(*stack)->next = node_to_swap;
-	}
+
+	node_to_swap = *stack;
+	temp2 = node_to_swap->n;
+	node_to_swap->n = node_to_swap->next->n;
+	node_to_swap->next->n = temp2;
 }
